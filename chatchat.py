cce084ce1182c329bad13974347152e0a405f842
ismailox1000 <34791551+ismailox1000@users.noreply.github.com>
2023-06-02 18:46:09 +0100

    Update chatchat.py

--- chatchat.py
+++ chatchat.py
@@ -14,15 +14,6 @@ st.markdown(
     "<h1 style='text-align: center; color: red;'>UK and Ireland DoS AI Test Finder BETA</h1>",
     unsafe_allow_html=True
 )
-with st.sidebar:
-    st.title('something here')
-    st.markdown('''
-    ## About
-        you can write here a description
-        here 
-
-    ''')
-
 
 @st.cache_data
 def get_img_as_base64(file):
@@ -44,42 +35,58 @@ page_bg_img = f"""
 [data-testid="stHeader"] {{
     background: rgba(0, 0, 0, 0);
 }}
+
+.sidebar .stMarkdown {{
+    height: calc(100vh - 8rem);
+    overflow-y: auto;
+}}
 [data-testid="stFormSubmitButton"]{{
-        width: 10%;
-    right: 10%;
+     width: 10%;
     bottom: 3rem;
     position: fixed;
     z-index: 1;
+    left:80%;
+}}
+[data-testid="stForm"]
+{{
+     width: 100%;
+    height:1%;
+    border:0px;
 }}
 
 </style>
 """
-
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-
+# Define the sidebar content
+st.sidebar.title('something here')
+st.sidebar.markdown('''
+    ## About
+    you can write here a description
+    here 
+''')
+if st.sidebar.button("Clear Chat"):
+    st.session_state["messages"] = get_initial_message()
+    st.session_state["past"] = []
+    st.session_state["generated"] = []
+# Initialize the QA chain
 os.environ["OPENAI_API_KEY"] = "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"
 persist_directory = 'db'
 
 def init():
     embedding = OpenAIEmbeddings()
-    vectordb = Chroma(persist_directory=persist_directory, 
-                      embedding_function=embedding)
+    vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
     retriever = vectordb.as_retriever()
 
-    qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0.2,streaming=True), 
-                                           chain_type="refine", 
-                                           retriever=retriever, 
+    qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0.2, streaming=True),
+                                           chain_type="refine",
+                                           retriever=retriever,
                                            return_source_documents=True)
     return qa_chain
 
-def process_llm_response(llm_response, prompt):
-    llm_response = qa_chain(prompt)
-    response = llm_response['result']
-    return response
-
 qa_chain = init()
 
+# Define the chat functionality
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
 if "past" not in st.session_state:
@@ -88,6 +95,7 @@ if "past" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state["messages"] = get_initial_message()
 
+# Styling for chat input and submit button
 styl = """
 <style>
 .stTextInput {
@@ -98,35 +106,23 @@ styl = """
     left: 30%;
 }
 
-.stButton > button {
-    width: 10%;
-    right: 10%;
-    bottom: 3rem;
-    position: fixed;
-    z-index: 1;
-}
 
-.sidebar .stMarkdown {
-    height: calc(100vh - 8rem); /* Adjust the height as needed */
-    overflow-y: auto;
-}
+.stButton > button:nth-child(2) {
+    right: 100;
+}  
 </style>
 """
 st.markdown(styl, unsafe_allow_html=True)
 
-
-
-# Get the user's question from a text input widget.
-
+# Chat input and submit button
 container = st.container()
 response_container = st.container()
 with container:
     with st.form(key='my_form', clear_on_submit=True):
-        
         query_text = st.text_input("Type Your Question:", key="input")
         submit_button = st.form_submit_button(label='Send')
 
-
+# Process user input and display chat messages
 if query_text and submit_button:
     with st.spinner("Generating..."):
         messages = st.session_state["messages"]
@@ -137,11 +133,9 @@ if query_text and submit_button:
         st.session_state.generated.append(response)  # Append the new generated message at the end
 
         # Display the chat messages with newest messages at the top
-        
         if len(st.session_state["generated"]) > 0:
             with response_container:
                 for i in range(len(st.session_state["generated"]) - 1, -1, -1):
                     index = len(st.session_state["generated"]) - i - 1  # Calculate the index for displaying messages
                     message(st.session_state["past"][index], is_user=True, key=str(uuid.uuid4()) + "_user")
                     message(st.session_state["generated"][index], key=str(uuid.uuid4()))
-        
